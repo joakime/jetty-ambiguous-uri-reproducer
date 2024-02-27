0d321cfaaf2e0dc45ee2c1e0d665cd9da6a24450
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2024-02-27 14:49:41 -0600

    Examples of URI construction

--- jetty12/src/test/java/jetty12/Jetty12Test.java
+++ jetty12/src/test/java/jetty12/Jetty12Test.java
@@ -1,6 +1,16 @@
 package jetty12;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -8,33 +18,82 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class Jetty12Test {
-
+public class Jetty12Test
+{
     @LocalServerPort
     private int port;
 
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Test
-    public void testFooBarBazEndpoint() {
-        URI url = UriComponentsBuilder.fromHttpUrl("http://localhost:" + port + "/foo=bar%2Fbaz=baz")
-                .encode()
-                .build()
-                .toUri();
-
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-
-        //FIXME: This should be false
-        assertTrue(response.toString().contains("Bad Request"));
-
-        //FIXME: Line below should pass as before
-//        assertTrue(response.toString().contains("%2F"));
+    public enum UriConstruction
+    {
+        JERSEY,
+        JERSEY_NOENCODE,
+        URI_DISTINCT_PARAMS,
+        URI_SSP_PARAMS,
+        URI_SINGLE_PARAM
     }
 
+    private URI newURI(UriConstruction uriConstruction) throws URISyntaxException
+    {
+        return switch (uriConstruction)
+        {
+            case JERSEY ->
+                // URI using Jersey techniques - This doesn't work, as this encodes the URI again.
+                UriComponentsBuilder.fromHttpUrl("http://localhost:" + port + "/foo=bar%2Fbaz=baz")
+                    .encode()
+                    .build()
+                    .toUri();
+            case JERSEY_NOENCODE ->
+                // URI using Jersey techniques, skipping .encode() - This doesn't work, as this STILL encodes the `%` in the URI again.
+                UriComponentsBuilder.fromHttpUrl("http://localhost:" + port + "/foo=bar%2Fbaz=baz")
+                    .build()
+                    .toUri();
+            case URI_DISTINCT_PARAMS ->
+                // URI in distinct parts - This doesn't work, as the path param is encoded going into URI
+                new URI("http", null, "localhost", port, "/foo=bar%2Fbaz=baz", null, null);
+            case URI_SSP_PARAMS ->
+                // URI using scheme specific part - This doesn't work, as the ssp is encoded going into the URI.
+                new URI("http", "//localhost:" + port + "/foo=bar%2Fbaz=baz", null);
+            case URI_SINGLE_PARAM ->
+                // This works, as you are providing the ENTIRE URI in one string.
+                new URI("http://localhost:" + port + "/foo=bar%2Fbaz=baz");
+        };
+    }
+
+    @ParameterizedTest
+    @EnumSource(UriConstruction.class)
+    public void testFooBarBazEndpointHttpClient(UriConstruction uriConstruction) throws IOException, InterruptedException, URISyntaxException
+    {
+        URI uri = newURI(uriConstruction);
+        assertThat("Oops, your URI technique is encoding the percent sign.", uri.toASCIIString(), not(containsString("%25")));
+
+        HttpClient httpClient = HttpClient.newBuilder().build();
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+            .uri(uri)
+            .POST(HttpRequest.BodyPublishers.noBody())
+            .build();
+        HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+
+        assertThat(httpResponse.body(), not(containsString("Bad Request")));
+        assertThat(httpResponse.body(), containsString("%2F"));
+    }
+
+    @ParameterizedTest
+    @EnumSource(UriConstruction.class)
+    public void testFooBarBazEndpointJersey(UriConstruction uriConstruction) throws URISyntaxException
+    {
+        URI uri = newURI(uriConstruction);
+        assertThat("Oops, your URI technique is encoding the percent sign.", uri.toASCIIString(), not(containsString("%25")));
+
+        ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
+        assertThat(response.toString(), not(containsString("Bad Request")));
+        assertThat(response.toString(), containsString("%2F"));
+    }
 }
